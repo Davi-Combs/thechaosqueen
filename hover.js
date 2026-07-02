9bd30bcf522480c936423ef202419c706e88b1b8
Davi Dawn <davidawncombs@gmail.com>
2026-07-02 02:03:31 +0000

    Create hover.js

---
+++ hover.js
@@ -0,0 +1,13 @@
+var element = 
+document.getElementByld("invisibleElement");
+
+element.addEventListener("mouseover",function(){
+
+element.classList.add("visible");
+//Make the element visible on mouseover
+));
+
+element.addEventListener("mouseout",function(){
+
+element.classList.remove("visible");// Remove visible class on mouseout
+});                      
